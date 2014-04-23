858c5672cc7e1e1472b7f2cc796b2edb211de250
Fachrul Pralienka BM <beeboy_pralienka@rocketmail.com>
2014-04-23 13:44:42 +0700

    Bab opat.txt dan save page 3

---
+++ Save Page 3/Continuous Integration in a Huge Multi-Module Project   c0nnexx10n   C0nnect1ng L1fe w1th Techn010gy_files/rate.php
@@ -0,0 +1,29 @@
+PDRTJS_368984_post_237.avg_rating = 0;
+PDRTJS_368984_post_237.votes = 0;		
+PDRTJS_settings_368984_post_237= {
+			"type" : "stars",
+			"size" : "sml",
+			"star_color" : "yellow",
+			"custom_star" : "",
+			"font_size" : "",
+			"font_line_height" : "16px",
+			"font_color" : "",
+			"font_align" : "left",
+			"font_position" : "right",
+			"font_family" : "",
+			"font_bold" : "normal",
+			"font_italic" : "normal",
+			"text_votes" : "Votes",
+			"text_rate_this" : "Rate This",
+			"text_1_star" : "Very Poor",
+			"text_2_star" : "Poor",
+			"text_3_star" : "Average",
+			"text_4_star" : "Good",
+			"text_5_star" : "Excellent",
+			"text_thank_you" : "Thank You",
+			"text_rate_up" : "Rate Up",
+			"text_rate_down" : "Rate Down"
+		};
+PDRTJS_368984_post_237.init();		
+PDRTJS_368984_post_237.token='7ee6754b5b17621fcf5ed08eadde1d86';
+/*368984,_post_237,wp-post-237,2024826842,0-0*/
